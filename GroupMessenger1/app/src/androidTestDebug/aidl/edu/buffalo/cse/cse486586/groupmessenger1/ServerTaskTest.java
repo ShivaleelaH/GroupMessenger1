cf336da666499cd3ab0ee2c9ad1f464f4af0ad32
ShivaleelaH <shivaleela.hubli1@gmail.com>
2020-05-25 22:56:36 -0400

    Add files via upload

---
+++ GroupMessenger1/app/src/androidTestDebug/aidl/edu/buffalo/cse/cse486586/groupmessenger1/ServerTaskTest.java
@@ -0,0 +1,12 @@
+package edu.buffalo.cse.cse486586.groupmessenger1;
+
+import junit.framework.TestCase;
+
+public class ServerTaskTest extends TestCase {
+
+    public void testDoInBackground() {
+    }
+
+    public void testOnProgressUpdate() {
+    }
+}
